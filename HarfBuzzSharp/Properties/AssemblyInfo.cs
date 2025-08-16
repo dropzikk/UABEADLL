5ecd4e3645c7cda73f405c2d3541e7259cb9e71c
dropzikk <160756491+dropzikk@users.noreply.github.com>
2025-08-16 17:37:23 +0300

    Add files via upload

---
+++ HarfBuzzSharp/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Diagnostics;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.Versioning;
+using System.Security;
+using System.Security.Permissions;
+
+[assembly: AssemblyTitle("HarfBuzzSharp")]
+[assembly: AssemblyDescription("HarfBuzzSharp is a cross-platform OpenType text shaping engine for .NET platforms.")]
+[assembly: AssemblyCompany("Microsoft Corporation")]
+[assembly: AssemblyProduct("HarfBuzzSharp")]
+[assembly: AssemblyCopyright("© Microsoft Corporation. All rights reserved.")]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: InternalsVisibleTo("SkiaSharp.Tests, PublicKey=002400000480000094000000060200000024000052534131000400000100010079159977d2d03a8e6bea7a2e74e8d1afcc93e8851974952bb480a12c9134474d04062447c37e0e68c080536fcf3c3fbe2ff9c979ce998475e506e8ce82dd5b0f350dc10e93bf2eeecf874b24770c5081dbea7447fddafa277b22de47d6ffea449674a4f9fccf84d15069089380284dbdd35f46cdff12a1bd78e4ef0065d016df")]
+[assembly: AssemblyMetadata("IsTrimmable", "True")]
+[assembly: AssemblyFileVersion("2.8.2.3")]
+[assembly: AssemblyInformationalVersion("2.8.2.3-655cff084fa9365efde6e989004eb818294c9b0f")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ HarfBuzzSharp/VersionConstants.cs
@@ -0,0 +1,14 @@
+internal class VersionConstants
+{
+	public const string AssemblyVersion = "1.0.0.0";
+
+	public const string AssemblyFileVersion = "2.8.2.3";
+
+	public const string AssemblyInformationalVersion = "2.8.2.3-655cff084fa9365efde6e989004eb818294c9b0f";
+
+	public const string GitSha = "-655cff084fa9365efde6e989004eb818294c9b0f";
+
+	public const int Milestone = 0;
+
+	public const int Increment = 0;
+}
